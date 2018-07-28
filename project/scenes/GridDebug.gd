74ec99074bf7cff21ef9f2244dc7bba1934acfb3
Todd Breiholz <tbreihol@meredith.com>
2018-07-28 14:10:32 -0500

    Debug grid working

--- project/scenes/GridDebug.gd
+++ project/scenes/GridDebug.gd
@@ -1,16 +1,23 @@
 extends Node2D
 
-const TILE_SIZE = Vector2(32, 32)
 
+const TILE_SIZE = Vector2(32, 32)
+export var LINE_COLOR = Color(1.0, 1.0, 1.0, 0.4)
+export var LINE_WIDTH = 2
+
+
+var last_pos = Vector2()
+var debug_rect = Rect2()
+
+	
 func _process(delta):
 	update()
 	
-func _draw():
-	var LINE_COLOR = Color(1.0, 1.0, 1.0, 0.4)
-	var LINE_WIDTH = 2
-	var player_pos = get_parent().position
-	player_pos.y += 16
-	var grid_pos = get_parent().get_parent().world_to_map(player_pos)
-	var rect = Rect2(to_local(grid_pos * TILE_SIZE), TILE_SIZE)
-	var a = 1
-	draw_rect(rect, LINE_COLOR, false)
+func _on_TileMap_pos_changed(grid_pos):
+	var rect = Rect2(grid_pos * TILE_SIZE, TILE_SIZE)
+	if grid_pos != last_pos:
+		self.position = (grid_pos + Vector2(1,1))* TILE_SIZE
+		self.visible = true
+		last_pos = grid_pos
+		print(grid_pos)
+
--- project/scenes/TileMap.gd
+++ project/scenes/TileMap.gd
@@ -1,12 +1,18 @@
 extends TileMap
 
-signal pos_changed
+signal pos_changed(grid_pos)
 
 var last_grid_pos = Vector2()
 const TILE_SIZE = 32
 
+func _ready():
+	self.connect("pos_changed", $GridDebug, "_on_TileMap_pos_changed")
+	
 func _process(delta):
-	var tm_pos = world_to_map($Player.position)
+	var player_pos = $Player.position
+	player_pos.x -= TILE_SIZE/2
+	#player_pos.y += TILE_SIZE/2
+	var tm_pos = world_to_map(player_pos)
 	if (tm_pos != last_grid_pos):
 		last_grid_pos = tm_pos
 		emit_signal("pos_changed", tm_pos)
